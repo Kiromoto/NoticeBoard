9ac5ed6705d0a1bf6de1d8b623c5fec9892c9403
root <root@DESKTOP-8GVH54M.localdomain>
2022-10-11 23:26:07 +0300

    NBoard: create models

--- NBoard/posts/models.py
+++ NBoard/posts/models.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
-class Post(models.Model):
+class Ad(models.Model):
     pass
 
 
@@ -10,4 +10,5 @@ class Comment(models.Model):
     pass
 
 
-class Author(models.Model)
+class Author(models.Model):
+    pass
